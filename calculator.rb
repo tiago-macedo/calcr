70928832c630095f11587a5b5b008df9a121688a
tiago-macedo <tiagomacedo@dcc.ufrj.br>
2021-04-02 17:56:02 -0300

    Lexer seems functional.
    Token class has two attributes: `lexeme` and `kind`. Both can be any string, but I expect kind to be either "number", "operator", "(" or ")".
    `Lexer.next` returns next token in string `Lexer.line`.
    Right now the lexer identifies integers and floats in non-scientific notation.

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,20 @@
 =begin
+
+/===============\
+| The language: |
+\===============/
+
+white_space : (' ' | '\t')* 
+
+integer    : [0-9]+
+float      : {integer}.{integer}
+scientific : ({integer} | {float}) (e | E) '-'? {integer}
+
+number     : {integer} | {float} | {scientific}
+operator   : '+' | '-' | '*' | '/'
+'('
+')'
+
 /==============\
 | The grammar: |
 \==============/
@@ -15,6 +31,7 @@ term -> term * fact
 
 fact -> NUM
       | '(' expr ')'
+
 =end
 
 class Calculator
@@ -38,17 +55,53 @@ class Lexer
 	end
 
 	def next
-		c = @line[@idx]
-		case c
-			when ' ', '\t', '\n'
-				whitespace
+		puts @idx
+		whitespace
+		p @idx
+		if @idx >= @line.length
+			return Token.new("", "end")
+		end
+		char = @line[@idx]
+		ascii = char.ord
+		if ascii >= 48 && ascii <= 57 # [0-9]
+			return number
+		end
+		@idx += 1
+		case char
+			when '+', '-', '*', '/'
+				return Token.new(char, "operator")
+			when '('
+				return Token.new(char, "(")
+			when ')'
+				return Token.new(char, ")")
 			else
-				puts "LEXER ERROR MESSAGE"
+				puts "LEXER ERROR: " + char
 		end
 	end
-
+	
+	def number
+		lexeme = integer
+		if @line[@idx] == '.'
+			lexeme << '.'
+			@idx += 1
+			lexeme << integer
+		end
+		return Token.new(lexeme, "number")
+	end
+	
+	def integer
+		lexeme = ""
+		ascii = @line[@idx].ord
+		while ascii >= 48 && ascii <= 57 # [0-9]
+			lexeme << @line[@idx]
+			@idx += 1
+			ascii = @line[@idx].ord
+		end
+		return lexeme
+	end
+	
 	def whitespace
-		while @line[@idx] == ' ' || @line[@idx] == '\t' || @line[@idx] == '\n'
+		while @line[@idx] == ' ' || @line[@idx] == "\t"
 			@idx += 1
 		end
 	end
@@ -63,7 +116,13 @@ class Token
 end
 
 lexer = Lexer.new
-lexer.line = "   a	"
-lexer.next
+string = "  1+399 * (4 + 7.5 -	3)	"
+lexer.line = string
+puts "length = " + string.length.to_s
+token = lexer.next
+while token.kind != "end"
+	puts "lexeme: " + token.lexeme + " | kind: " + token.kind
+	token = lexer.next
+end
 puts "line: \"" + lexer.line + "\""
 puts "idx:    " + lexer.idx.to_s
